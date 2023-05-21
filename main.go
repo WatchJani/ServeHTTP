c38068ad2e39b010ce48d2730f48b1e53445900b
WatchJani <jankokondic84@gmail.com>
2023-05-21 17:57:12 +0200

    dynamic routing implementation added

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+var NotExistPath = ""
+
 type Router struct {
 	routeEvent string
 	route      map[string]http.HandlerFunc
@@ -31,16 +33,27 @@ func (r *Router) MyHandler(path string, handler http.HandlerFunc) {
 }
 
 func Req(w http.ResponseWriter, r *http.Request) {
-	// params := r.Context().Value("params")
 	params := Params(r)
 
-	fmt.Println(params)
+	fmt.Println(params["id"])
 }
 
 func Params(r *http.Request) map[string]string {
 	params := make(map[string]string)
 
-	r.Context().Value("params")
+	url := strings.Join([]string{r.Context().Value("params").(string)[1:], r.URL.Path}, "")
+
+	sliceURL := strings.Split(url, "/")
+
+	// fmt.Println(sliceURL[:4])
+	// fmt.Println(sliceURL)
+	// fmt.Println(len(sliceURL))
+
+	for index := 0; index < len(sliceURL)/2; index++ {
+		if sliceURL[index][0] == ':' {
+			params[sliceURL[index][1:]] = sliceURL[index+len(sliceURL)/2]
+		}
+	}
 
 	return params
 }
@@ -52,7 +65,7 @@ func DefaultURL(dynamicURL, reqURL string) string {
 		return dynamicURL
 	}
 
-	return ""
+	return NotExistPath
 }
 
 func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
@@ -70,7 +83,7 @@ func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 func main() {
 	router := New()
 
-	router.MyHandler("/user/:id", Req)
+	router.MyHandler("/user/:id/team/:color", Req)
 
 	http.ListenAndServe(":5000", router)
 }
