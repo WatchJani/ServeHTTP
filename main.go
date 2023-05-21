0317f6854f4f2d1376eb814cbfe34ea971351f3b
WatchJani <jankokondic84@gmail.com>
2023-05-21 02:31:12 +0200

    dynamic router

--- main.go
+++ main.go
@@ -1,24 +1,55 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 )
 
 type Router struct {
+	route map[string]http.HandlerFunc
+}
+
+func New() *Router {
+	return &Router{
+		route: make(map[string]http.HandlerFunc),
+	}
+}
+
+func (r *Router) MyHandler(path string, handler http.HandlerFunc) {
+	r.route[path] = handler
+}
+
+func Req(w http.ResponseWriter, r *http.Request) {
+	// params := r.Context().Value("params")
+	params := Params(r)
+
+	fmt.Println(params)
+}
+
+func Params(r *http.Request) map[string]string {
+	params := make(map[string]string)
+
+	r.Context().Value("params")
+
+	params["top"] = "top"
+
+	return params
 }
 
 func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("req")
+	if fn, ok := r.route[req.URL.Path]; ok {
+		ctx := context.WithValue(req.Context(), "params", "janko")
+		fn(w, req.WithContext(ctx))
+	}
 
-	fmt.Println(req.URL.Path)
-
-	// Route not found
-	// http.NotFound(w, req)
+	fmt.Println("ne postoji")
 }
 
 func main() {
-	router := &Router{}
+	router := New()
+
+	router.MyHandler("/user", Req)
 
 	http.ListenAndServe(":5000", router)
 }
